14ac6a9619adb1c170d822fd7d6df0486c88b9ab
Jesse McKinney <jessman51386@gmail.com>
2020-04-28 20:58:34 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -9,6 +9,33 @@ import ErrorPage from "./pages/ErrorPage/Error";
 import "./App.css";
 require("dotenv").config();
 
+// var context;
+
+// window.onload = function () {
+//   context = new AudioContext();
+//   // Setup all nodes
+//   console.log(context.state);
+// };
+
+// if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//   console.log("getUserMedia supported.");
+//   navigator.mediaDevices
+//     .getUserMedia(
+//       // constraints - only audio needed for this app
+//       {
+//         audio: true,
+//       }
+//     )
+//     // Success callback
+//     .then(function (stream) {})
+//     // Error callback
+//     .catch(function (err) {
+//       console.log("The following getUserMedia error occured: " + err);
+//     });
+// } else {
+//   console.log("getUserMedia not supported on your browser!");
+// }
+
 function App() {
   return (
     <Router>
