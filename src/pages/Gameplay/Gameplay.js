4e23aa93c57cb6ab26209750882754c20866ee53
Jesse McKinney <jessman51386@gmail.com>
2020-04-30 11:01:20 -0400

    yay

--- src/pages/Gameplay/Gameplay.js
+++ src/pages/Gameplay/Gameplay.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import "./Gameplay.css";
 
 class Gameplay extends Component {
   render() {
     return (
       <>
-        <div></div>
+        <div id="gameplayWrapper">
+          <Container>
+            <Row>
+              <Col>
+                <div id="gameplayTitle">How To Play:</div>
+              </Col>
+            </Row>
+          </Container>
+
+          <Container>
+            <Row>
+              <Col>
+                <Link to="/">
+                  <Button>Home</Button>
+                </Link>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </>
     );
   }
