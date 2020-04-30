43e13f7fb25b1f5f74d8255e7373d94fdf6888f6
Jesse McKinney <jessman51386@gmail.com>
2020-04-30 13:02:24 -0400

    yay

--- src/pages/MultiPlayer/MultiPlayer.js
+++ src/pages/MultiPlayer/MultiPlayer.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import "./MultiPlayer.css";
 
 class MultiPlayer extends Component {
   render() {
     return (
       <>
-        <div></div>
+        <div id="multiplayerWrapper">
+          <Container>
+            <Row>
+              <Col>
+                <div id="multiplayerTitle">Coming Soon!!!</div>
+              </Col>
+            </Row>
+          </Container>
+          <Container>
+            <Row>
+              <Col>
+                <Link to="/">
+                  <Button>Home</Button>
+                </Link>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </>
     );
   }
