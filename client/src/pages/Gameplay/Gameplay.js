08a5e17dceb8adfc17d2d69e48cfb4e5806798fb
Jesse McKinney <jessman51386@gmail.com>
2020-05-05 14:00:03 -0400

    yay

--- client/src/pages/Gameplay/Gameplay.js
+++ client/src/pages/Gameplay/Gameplay.js
@@ -19,15 +19,32 @@ class Gameplay extends Component {
             </Row>
             <Row id="gameplayParagraphRow">
               <Col id="gameplayParagraphCol">
-                <p id="gameplayParagraph"></p>
+                <p id="gameplayParagraph">
+                  The game begins immediately when the Start button is pressed.
+                  When the screen loads, press the New Word button to see a
+                  random word. Think of a song that has that random word in its
+                  lyrics. Press the red Record button to start recording. While
+                  the game is recording, sing the song that has the random word
+                  included in the lyrics. Press the red Record button again to
+                  turn off the recording session. The green Submit button will
+                  then become enabled, allowing the player to submit the song of
+                  their choice. Upon submit, the player will then see the song
+                  they chose to sing and the songs lyrics, if they are correct.
+                  Please speak clearly and loud enough into your microphone
+                  while recording. For accuracy, please sing at least 8 words.
+                  The more words matched to the lyrics, the more accurate of a
+                  result.
+                </p>
               </Col>
             </Row>
-            <Row id="playerButtonRow">
+            <Row id="singlePlayerButtonRow">
               <Col id="singleplayerButtonCol">
                 <Link to="/singleplayer">
                   <Button id="singleplayerButton">Single Player Game</Button>
                 </Link>
               </Col>
+            </Row>
+            <Row id="multiPlayerButtonRow">
               <Col id="multiplayerButtonCol">
                 <Link to="/multiplayer">
                   <Button id="multiplayerButton">Multi-Player Game</Button>
